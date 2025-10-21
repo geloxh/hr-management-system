47fd67a7270d82be847a5864aa73b6aa192e52f6
geloxh <ace.blazado7@gmail.com>
2025-10-21 20:42:08 +0800

    commit update

--- profile.php
+++ profile.php
@@ -26,3 +26,28 @@
     $cityId = $row['CityId'];
     $citId = mysqli_query($db, "SELECT * FROM city WHERE CityId = '$cityId'");
     $city = mysqli_fetch_assoc($citId);
+
+    $stateId = $city['StateId'];
+    $staId = mysqli_query($db, "SELECT * FROM STATE WHERE StateId = '$stateId'");
+    $state = mysqli_fetch_assoc($staId);
+
+    $countryId = $state['CountryId'];
+	$couId = mysqli_query($db, "SELECT * FROM country WHERE CountryId = '$countryid'");
+	$country = mysqli_fetch_assoc($couid);
+
+	$positionId = $row['PositionId'];
+	$posId = mysqli_query($db, "SELECT * FROM position WHERE PositinId = '$positionId'");
+	$position = mysqli_fetch_assoc($posId);
+
+	$roleId = $row['RoleId'];
+	$rolId= mysqli_query($db, "SELECT * FROM ROLE WHERE RoleId = '$rolId'");
+	$role = mysqli_fetch_assoc($rolId);
+
+    if(isset($_POST['close']))
+	{
+		echo "<script>window.location='home.php';</script>";
+	}
+?>
+
+<link href="css/table-style.css" rel="stylesheet" type="text/css" />
+<script src="
