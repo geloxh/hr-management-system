fdc063e9ffdd933c5f013b91db54bb67e2343abb
geloxh <ace.blazado7@gmail.com>
2025-12-01 21:33:06 +0800

    footer

---
+++ ins/footer.php
@@ -0,0 +1,8 @@
+    <footer>
+        <div class="container">
+            <p>&copy; 2025 HR Management System</p>
+        </div>
+    </footer>
+    <script src="scs/a.js"></script>
+</body>
+</html>
