3884372a2f86a796a5b0e95f5b55a0775ff0bc4d
geloxh <ace.blazado7@gmail.com>
2025-12-01 21:30:08 +0800

    header

---
+++ ins/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="sss">
+    <title>HR Management System</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <header>
+        <nav class="container">
+            <a href="index.php" class="logo">HRMS</a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+            </ul>
+        </nav>
+    </header>
