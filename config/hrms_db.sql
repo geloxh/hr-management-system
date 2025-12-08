c14bce266cd15a8b9d836299a3f411bdeb780aac
geloxh <ace.blazado7@gmail.com>
2025-12-08 20:46:42 +0800

    db update

--- config/hrms_db.sql
+++ config/hrms_db.sql
@@ -1,5 +1,10 @@
 -- HR MANAGEMENT SYSTEM DATABASE
 
+--
+-- Database: `hrms_db.sql`
+--
+
+-- --------------------------------------------------------
 -- Create Database
 CREATE DATABASE IF NOT EXISTS hrms_db;
 USE hrms_db;
@@ -34,6 +39,76 @@ INSERT INTO `assets` (`id`, `assetName`, `assetId`, `PurchaseDate`, `PurchaseFro
 (1, 'AIO', '', '2020-09-23', 'Amazon', 'Apple Inc.', '2020', 1, 'Amazon', 'In good shape', '12 Months', 1900, 'Mushe abdul-Hakim', 'This is the description of the laptop', '2020-09-23 23:57:26');
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(225) NOT NULL,
+  `ClientId` varchar(225) NOT NULL,
+  `Phone` varchar(20) NOT NULL,
+  `Company` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `Status` int(11) NOT NULL,
+  `Picture` varchar(225) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `FirstName`, `LastName`, `UserName`, `Email`, `Password`, `ClientId`, `Phone`, `Company`, `Address`, `Status`, `Picture`, `date`) VALUES
+(1, 'Yahuza', 'Abdul-Hakim', 'Vendetta', 'musheabdulhakim@protonmail.ch', '$2y$10$xU1zDRigag7ZMGs0Egcqoei0SrtZJRX/p425h4qOi5OMKFz32k0UC', 'CLT-613498', '233209229025', 'Microsoft Inc', 'Live from home', 1, 'd41d8cd98f00b204e9800998ecf8427e1601112041', '2020-09-26'),
+(2, 'Vendetta', 'Alkaline', 'alkaline', 'musheabdulhakim99@gmail.com', '$2y$10$qUL2APr762X.vvJuNQvqludvabDa.Y3TRHOa.M/qq8WFoeoh7IaWG', 'CLT-217594', '233209229025', 'Falcon Systems', 'Live from home', 1, 'd41d8cd98f00b204e9800998ecf8427e1601112339', '2020-09-26');
+
+-- --------------------------------------------------------
+
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL,
+  `Department` varchar(200) NOT NULL,
+  `Date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `Department`, `Date`) VALUES
+(2, 'Marketing', '2020-09-26'),
+(3, 'IT Department', '2020-09-26'),
+(4, 'Web Development', '2020-09-27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designations`
+--
+
+CREATE TABLE `designations` (
+  `id` int(11) NOT NULL,
+  `Designation` varchar(100) NOT NULL,
+  `Department` varchar(100) NOT NULL,
+  `Date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `designations`
+--
+
+INSERT INTO `designations` (`id`, `Designation`, `Department`, `Date`) VALUES
+(1, 'Web Designer', 'Web Development', '2020-09-27'),
+(2, 'Web Developer', 'Web Development', '2020-09-27');
+
+-- --------------------------------------------------------
+
+
 -- User Table (Admin + HR Officers)
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
