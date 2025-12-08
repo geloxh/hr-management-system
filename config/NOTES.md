6a286c190fff39f41a5c792e6b5efef4ba057566
geloxh <ace.blazado7@gmail.com>
2025-12-08 20:43:22 +0800

    db update

---
+++ config/NOTES.md
@@ -0,0 +1,3 @@
+### CONFIG FILES
+This directory contains the `db.php` file that contains the database connection details. KTKhe `hrms_db.sql` file
+is the SQL file that contains the database schema.
--- config/hrms_db.sql
+++ config/hrms_db.sql
@@ -4,6 +4,36 @@
 CREATE DATABASE IF NOT EXISTS hrms_db;
 USE hrms_db;
 
+--
+-- Table structure for table `assets`
+--
+
+CREATE TABLE `assets` (
+  `id` int(11) NOT NULL,
+  `assetName` varchar(200) NOT NULL,
+  `assetId` varchar(200) NOT NULL,
+  `PurchaseDate` date NOT NULL,
+  `PurchaseFrom` varchar(200) NOT NULL,
+  `Manufacturer` varchar(200) NOT NULL,
+  `Model` varchar(200) NOT NULL,
+  `Status` int(10) NOT NULL,
+  `Supplier` varchar(255) NOT NULL,
+  `AssetCondition` varchar(255) NOT NULL,
+  `Warranty` varchar(255) NOT NULL,
+  `Price` int(255) NOT NULL,
+  `AssetUser` varchar(255) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `DateTime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `assets`
+--
+
+INSERT INTO `assets` (`id`, `assetName`, `assetId`, `PurchaseDate`, `PurchaseFrom`, `Manufacturer`, `Model`, `Status`, `Supplier`, `AssetCondition`, `Warranty`, `Price`, `AssetUser`, `Description`, `DateTime`) VALUES
+(1, 'AIO', '', '2020-09-23', 'Amazon', 'Apple Inc.', '2020', 1, 'Amazon', 'In good shape', '12 Months', 1900, 'Mushe abdul-Hakim', 'This is the description of the laptop', '2020-09-23 23:57:26');
+
+-- --------------------------------------------------------
 -- User Table (Admin + HR Officers)
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
